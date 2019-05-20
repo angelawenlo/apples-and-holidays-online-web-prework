2ba401ced455c570e4d52e3fa787f3bfd9a86fda
Angela Lo <angelawenlo@gmail.com>
2019-05-20 13:38:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{seasons.to_s.capitalize!}:"
     holidays.each do |holiday, supplies|
      holiday.to_s.split.each do |h|
-    puts  h.capitalize.join("")
+    puts h.capitalize.join("")
 
    end
 end
