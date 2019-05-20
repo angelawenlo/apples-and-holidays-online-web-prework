8cbb5218918feadbdff3a6691d420e8662505819
Angela Lo <angelawenlo@gmail.com>
2019-05-20 14:19:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_holidays_with_bbq(holiday_hash)
     holiday_hash.map do |seasons, holidays|
     holidays.map do |holiday, supplies|
       if supplies.include?("BBQ")
-        puts holiday
+        holiday
       end
     end
     end
